ca6959536418e1e118b4ebdde8b4232c534c2041
James BB <jbridgebutler@gmail.com>
2017-08-06 18:20:10 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *  main handling file (controller)
+ */
+
+/**
+ *  include some external library files and class definitions
+ */
+
+require_once('php.extend.php);
+
+
+/**
+ *  what we do here is take the request from the url and decide what we're going to server back to the user.
+ */
+
+
+
+
+
